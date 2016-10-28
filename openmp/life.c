d8460f3647aa4b9abc8d455298facb8b4c9f35b2
egiby <egiby@egiby>
2016-10-29 01:56:16 +0300

    openmp simple life added

---
+++ openmp/life.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include <omp.h>
+
+#include "../common/board.h"
+#include "../common/test.h"
+#include "../common/common.h"
+
+int main()
+{
+    FILE * file = fopen("../oscillator", "r");
+    
+    int n, m, k;
+    char is_reversed = 0;
+    fscanf(file, "%d %d %d\n", &n, &m, &k);
+    
+    Board * board = get_board(n, m);
+    read_board(board, file);
+    
+    if (n < m)
+    {
+        is_reversed = 1;
+        reverse_board(board);
+        swap_int(&n, &m);
+    }
+    
+    Board * new_board = get_board(n, m);
+    for (int i = 0; i < k; ++i)
+#pragma omp parallel shared(board, new_board, n, m)
+    {
+    #pragma omp barrier
+        int left = calc_left(n, omp_get_thread_num(), omp_get_num_threads());
+        int right = calc_right(n, omp_get_thread_num(), omp_get_num_threads());
+        
+        for (int x = left; x < right; ++x)
+            for (int y = 0; y < m; ++y)
+            {
+                int cnt = 0;
+                for (int j = 0; j < 8; ++j)
+                    cnt += get_elem(board, x + dx[j], y + dy[j]);
+                
+                new_board->board[x][y] = get_life_value(cnt, get_elem(board, x, y));
+            }
+    #pragma omp barrier
+    #pragma omp single
+        swap_iters2(&new_board->board, &board->board);
+    }
+    
+    if (is_reversed)
+    {
+        reverse_board(board);
+        swap_int(&n, &m);
+    }
+    
+    print_board(board);
+    
+    delete_board(board);
+    delete_board(new_board);
+    return 0;
+}
--- openmp/sum.c
+++ openmp/sum.c
@@ -16,18 +16,13 @@ int main()
     for (int i = 0; i < n; ++i)
         scanf("%d", &a[i]);
     
-#pragma omp parallel
+#pragma omp parallel reduction (+:s)
     {
-        int local_sum = 0;
-        
         int left = calc_left(n, omp_get_thread_num(), omp_get_num_threads());
         int right = calc_right(n, omp_get_thread_num(), omp_get_num_threads());
         
         for (int i = left; i < right; ++i)
-            local_sum += a[i];
-        
-    #pragma omp atomic
-        s += local_sum;
+            s += a[i];
     }
     
     printf("%lld\n", s);
